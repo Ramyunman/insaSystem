e425218d8df3ee1c2e713ea6b7d1240fe020492f
Ramyunman <100078669+Ramyunman@users.noreply.github.com>
2023-12-19 14:47:09 +0900

    231219 insaSystem 작성중

---
+++ src/main/java/kr/co/vo/BoardVO.java
@@ -0,0 +1,87 @@
+package kr.co.vo;
+
+import java.sql.Date;
+
+public class BoardVO {
+
+	private int employee_id;
+	private String name;
+	private String email;
+	private String phone_number;
+	private Date hire_date;
+	private Date leave_date;
+	private int department_id;
+	private String address;
+	private long reg_no;
+	private int bank_id;
+	private String bank_account;
+	
+	public int getEmployee_id() {
+		return employee_id;
+	}
+	public void setEmployee_id(int employee_id) {
+		this.employee_id = employee_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	public Date getHire_date() {
+		return hire_date;
+	}
+	public void setHire_date(Date hire_date) {
+		this.hire_date = hire_date;
+	}
+	public Date getLeave_date() {
+		return leave_date;
+	}
+	public void setLeave_date(Date leave_date) {
+		this.leave_date = leave_date;
+	}
+	public int getDepartment_id() {
+		return department_id;
+	}
+	public void setDepartment_id(int department_id) {
+		this.department_id = department_id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public long getReg_no() {
+		return reg_no;
+	}
+	public void setReg_no(long reg_no) {
+		this.reg_no = reg_no;
+	}
+	public int getBank_id() {
+		return bank_id;
+	}
+	public void setBank_id(int bank_id) {
+		this.bank_id = bank_id;
+	}
+	public String getBank_account() {
+		return bank_account;
+	}
+	public void setBank_account(String bank_account) {
+		this.bank_account = bank_account;
+	}
+	
+	
+}
